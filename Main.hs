1c417caa46f57f01e96d468e2be1095088c630c9
Ian Duncan <iand675@gmail.com>
2011-08-03 13:37:36 -0500

    Can load modules in the tasks dir of the current directory. Currently hardcoded to execute the Zoom.SayHello.hi function as a functionality test.

--- Main.hs
+++ Main.hs
@@ -2,9 +2,19 @@ module Main where
 import Language.Haskell.Interpreter
 import Control.Monad
 import Control.Monad.Trans
-  
+import System.Directory
+import System.FilePath
+import Data.Monoid
+import qualified Data.List as L
+ifM :: (Monad m, Monoid md) => m Bool -> md -> m md
+ifM m x = do
+  result <- m
+  return $ if result
+           then x
+           else mempty
+         
 main = do 
-  result <- runInterpreter test
+  result <- runInterpreter (loadTaskModules >> test)
   putStrLn $ show result
 
 isFunction x = case x of 
@@ -16,12 +26,38 @@ msg x = liftIO . putStrLn . show $ x
 files = ["/Users/ian/Code/zoom/Main.hs"]
 test :: Interpreter ()
 test = do
-  loadModules files
-  setTopLevelModules ["Main"]
   modules <- getLoadedModules
   exports <- mapM getModuleExports modules
   let fs = filter isFunction $ join exports
       fnames = map name fs
   msg fs
-  mainType <- typeOf "main"
-  msg mainType
+  -- mainType <- typeOf "main"
+  hi <- interpret "SayHello.hi" (as :: IO ())
+  liftIO hi
+
+-- get current working directory
+-- TODO recurse all the way to home, getting tasks for each level.
+-- TODO also, get them from some global location
+getTaskDirs = do
+  current <- getCurrentDirectory
+  let taskDir = current </> "tasks"
+  ifM (doesDirectoryExist taskDir) [taskDir]
+
+getAndQualifyContents dir = do
+  contents <- getDirectoryContents dir
+  let realContents = filter (`notElem` [".", ".."]) contents
+  return $ map (dir </>) realContents
+  
+loadTaskModules :: Interpreter ()
+loadTaskModules = do
+  dirs           <- liftIO getTaskDirs
+  allDirPaths    <- liftIO $ mapM getAndQualifyContents dirs
+  allModulePaths <- liftIO $ filterM (fmap not . doesDirectoryExist) $ join allDirPaths
+  loadModules allModulePaths
+  allModules     <- getLoadedModules
+  let 
+    zoomModules      = filter (L.isPrefixOf "Zoom.") allModules
+    qualifyModule x  = L.stripPrefix "Zoom." x
+    qualifiedModules = zip zoomModules (map qualifyModule zoomModules)
+  setImportsQ qualifiedModules
+--  setTopLevelModules zoomModules
---
+++ tasks/SayHello.hs
@@ -0,0 +1,5 @@
+module Zoom.SayHello where
+
+hi = putStrLn "hi"
+  
+  
