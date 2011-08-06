a67041317ad404150ee4d682b02d8e0f6e05ce66
Ian Duncan <iand675@gmail.com>
2011-08-05 22:41:31 -0500

    Improved error messages

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 module Main where
+import Language.Haskell.Interpreter
 import Zoom.Interpreter
 -- import System.Console.GetOpt         
 import Zoom.Task
@@ -8,5 +9,11 @@ main = do
   args <- getArgs
   result <- runZoomInterpreter $ map Args args
   case result of
-       Left err -> putStrLn $ show err
+       Left err -> case err of
+         UnknownError ue -> putStrLn ("Something went wrong, but I can't tell what happened. Here's all I know: " ++ ue)
+         WontCompile errs -> do 
+           putStrLn "Interpreting Error(s):"
+           mapM_ (putStrLn . errMsg) errs                     
+         NotAllowed str -> putStrLn $ "You can't do this: " ++ str
+         GhcException e -> putStrLn $ "GHC Exception: " ++ e
        Right _  -> return ()
